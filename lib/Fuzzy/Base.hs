21742325926b2521b4fbabcc3a23e157675dc2e6
Maksym Syvash <maxim.sivash@gmail.com>
2024-10-21 23:31:35 +0300

    MF return type is now parametrized

--- lib/Fuzzy/Base.hs
+++ lib/Fuzzy/Base.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE TypeFamilies #-}
 
 module Fuzzy.Base where
+
 class Fuzzy f e where
     type Crisp e
+    type Returned f e
     supp :: f e -> Crisp e
-    is :: e -> f e -> Double
-    height :: f e -> Double
+    is :: e -> f e -> Returned f e
+    height :: f e -> Returned f e
     core :: f e -> Crisp e
-    alphacut :: f e -> Crisp e
+    alphacut :: f e -> Returned f e -> Crisp e
     mode :: f e -> Crisp e
 
 class FuzzySetOps a where
