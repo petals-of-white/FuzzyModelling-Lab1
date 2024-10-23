6d13ef26cbe36957ef29872a3b9a8c428a0de0bd
Maksym Syvash <maxim.sivash@gmail.com>
2024-10-23 19:49:23 +0300

    Squashed commit of the following:
    commit 92f03f0a4d54dd312fc77c8241f404216b29e3cd
    Author: Maksym Syvash <maxim.sivash@gmail.com>
    Date:   Wed Oct 23 19:38:15 2024 +0300
    
        Added finite universum

--- lib/Fuzzy/MF.hs
+++ lib/Fuzzy/MF.hs
@@ -3,26 +3,25 @@ module Fuzzy.MF where
 import           Fuzzy.Base
 
 -- | Алгебраїчна
-newtype AlgebraMF a = AlgebraMF (a -> Double)
+newtype AlgebraMF m a = AlgebraMF (a -> m)
 
 -- | Мінімаксна
-newtype MaxMinMF a = MaxMinMF (a -> Double)
+newtype MaxMinMF m a = MaxMinMF (a -> m)
 
 -- | Обмежена
-newtype BoundedMF a = BoundedMF (a -> Double)
+newtype BoundedMF m a = BoundedMF (a -> m)
 
-instance FuzzySetOps (AlgebraMF a) where
+instance (Fractional m, Ord m) => FuzzySetOps (AlgebraMF m a) where
   (AlgebraMF f1) ?|| (AlgebraMF f2) = AlgebraMF (\e -> f1 e + f2 e - f1 e * f2 e)
   (AlgebraMF f1) ?&& (AlgebraMF f2) = AlgebraMF (\e -> f1 e * f2 e)
   fnot (AlgebraMF f) = AlgebraMF (\e -> 1 - f e)
 
-instance FuzzySetOps (MaxMinMF a) where
+instance (Fractional m, Ord m) => FuzzySetOps (MaxMinMF m a) where
   (MaxMinMF f1) ?|| (MaxMinMF f2) = MaxMinMF (\e -> max (f1 e) (f2 e))
   (MaxMinMF f1) ?&& (MaxMinMF f2) = MaxMinMF (\e -> min (f1 e) (f2 e))
   fnot (MaxMinMF f) = MaxMinMF (\e -> 1 - f e)
 
-instance FuzzySetOps (BoundedMF a) where
+instance (Fractional m, Ord m) => FuzzySetOps (BoundedMF m a) where
   (BoundedMF f1) ?|| (BoundedMF f2) = BoundedMF (\e -> min 1 (f1 e + f2 e))
   (BoundedMF f1) ?&& (BoundedMF f2) = BoundedMF (\e -> max 0 (f1 e + f2 e - 1))
-  fnot :: BoundedMF a -> BoundedMF a
   fnot (BoundedMF f) = BoundedMF (\e -> 1 - f e)
