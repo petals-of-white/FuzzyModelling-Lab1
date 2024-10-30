9fbbb342d1f2e44cb66b63c5def16ff2bc98d45e
Maksym Syvash <maksym_syvash@outlook.com>
2024-10-30 19:40:54 +0200

    Update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -20,32 +20,55 @@ setup window = do
         UI.addStyleSheet window "main.css"
         _ <- return window # set UI.title "Лабораторна робота №1"
         gLabel <- UI.label # set UI.text "g = " # set UI.for "g"
-        gInput <- UI.input # set UI.type_ "number" # set UI.name "g" # set SVG.min "0" # set SVG.max "9"
+        gInput <- UI.input # set UI.type_ "number" # set UI.id_ "g" # set UI.name "g" # set SVG.min "0" # set SVG.max "9"
         kLabel <- UI.label # set UI.text "k = " # set UI.for "k"
-        kInput <- UI.input # set UI.type_ "number" # set UI.name "k" # set SVG.min "0" # set SVG.max "9"
+        kInput <- UI.input # set UI.type_ "number" # set UI.id_ "k" # set UI.name "k" # set SVG.min "0" # set SVG.max "9"
         vValue <- UI.label # set UI.text "V = "
 
-
+        -- TODO: set type
         calcButton <- UI.button # set UI.text "Обчислити"
+
+        -- TODO: set title
         arithopselect <- UI.listBox (pure (enumFromTo Add Div)) (pure $ Just Sub) (pure $ \op -> UI.label # set UI.text (show op))
 
 
+        setOpsSection <- UI.div
+
+        setsAB <- UI.div
+
         setsInfo <- UI.div
+
+
         _ <- getBody window #+ [
-            element gLabel, element gInput,
-            element kLabel, element kInput,
-            element vValue,
-            element calcButton, element arithopselect, element setsInfo]
+            UI.header #+ [UI.h1 # set UI.text "Лабораторна робота №1. Рівас Сіваш Максим ЗК-31мн"],
+            UI.div #+ [
+                UI.div #+ [element gLabel, element gInput,
+                element kLabel, element kInput, element vValue],
 
-        on UI.click calcButton $ const $ do
-            g <- gInput # get UI.value
-            k <- kInput # get UI.value
-            let v = calcV (read g) (read k)
+                element calcButton, element arithopselect,
+                element setsAB,
+                element setsInfo,
+                element setOpsSection
 
-            vValue # set' UI.text ("V = " ++ show v)
+                ]
+            ]
 
-            sets <- displayAB v
-            _ <- element setsInfo # set UI.children [sets]
+        -- on UI.click calcButton $ const $ do
+        -- g <- gInput # get UI.value
+        -- k <- kInput # get UI.value
+        let g = "2"
+            k = "5"
+        let v = calcV (read g) (read k)
+            setA = makeA v
+            setB = makeB v
 
-            element calcButton # set UI.text "I have been clicked!"
+        vValue # set' UI.text ("V = " ++ show v)
+
+        sets <- displayAB setA setB
+        _ <- element setOpsSection #+ [setOps setA setB v]
+        _ <- element setsAB # set UI.children [sets]
+        _ <- element setsInfo #+ [displaySetInfo setA setB]
+
+        _ <- element calcButton # set UI.text "I have been clicked!"
+        return ()
 
