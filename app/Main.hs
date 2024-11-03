963df6a25f07bfe5c45447a041104eb650b12d50
Maksym Syvash <maksym_syvash@outlook.com>
2024-11-03 08:17:32 +0200

    fix ui with 'Pure' css framework

--- app/Main.hs
+++ app/Main.hs
@@ -16,9 +16,17 @@ main = do
     jsStatic = Just staticPath
 } setup
 
+
 setup :: Window -> UI ()
 setup window = do
         UI.addStyleSheet window "main.css"
+        _ <- UI.getHead window #+ [
+                UI.meta # set UI.name "viewport" # set UI.content "width=device-width, initial-scale=1" ,
+                UI.link
+                    # set UI.rel "stylesheet" # set UI.href "https://cdn.jsdelivr.net/npm/purecss@3.0.0/build/pure-min.css"
+                    # set (UI.attr "integrity") "sha384-X38yfunGUhNzHpBaEBsWLO+A0HDYOQi8ufWDkZ0k9e0eXz/tH3II7uKZ9msv++Ls"
+                    # set (UI.attr "crossorigin") "anonymous"
+            ]
         _ <- return window # set UI.title "Лабораторна робота №1"
         gLabel <- UI.label # set UI.text "g = " # set UI.for "g"
         gInput <- UI.input # set UI.type_ "number" # set UI.id_ "g" # set UI.name "g" # set SVG.min "0" # set SVG.max "9"
@@ -47,18 +55,19 @@ setup window = do
 
         _ <- getBody window #+ [
             Widgets.appHeader,
-            UI.div #+ [
-                UI.div #+ [
+            UI.mkElement "main" # set UI.id_ "main" #. "pure-g" #+ [
+                UI.div # set UI.id_ "displayABSection" #. "pure-u-1-1" #+ [
                     element gLabel, element gInput,
-                    element kLabel, element kInput, element vValue
+                    element kLabel, element kInput,
+                    element vValue
                 ],
 
-                getElement <$> Widgets.fuzzyAB ((\VariantData {varSetA=a, varSetB=b} ->(a,b)) <$> validVarInput),
-                getElement <$> Widgets.displaySetInfo validVarInput,
-                getElement <$> Widgets.setOps validVarInput,
-                Widgets.triangleFuzzyNumberInfo (varTriangles <$> validVarInput),
-                Widgets.trapeziumFuzzyNumberInfo (varTrapezia <$> validVarInput),
-                Widgets.arithOpSection validVarInput
+                (getElement <$> Widgets.fuzzyAB ((\VariantData {varSetA=a, varSetB=b} ->(a,b)) <$> validVarInput)) #. " pure-u-1-1",
+                (getElement <$> Widgets.displaySetInfo validVarInput) #. "pure-table pure-table-bordered pure-u-1-1",
+                (getElement <$> Widgets.setOps validVarInput) #. "pure-u-1-1",
+                Widgets.triangleFuzzyNumberInfo (varTriangles <$> validVarInput) #. "pure-u-1-2",
+                Widgets.trapeziumFuzzyNumberInfo (varTrapezia <$> validVarInput) #. "pure-u-1-2",
+                Widgets.arithOpSection validVarInput #. "pure-table pure-table-bordered pure-u-1-1"
                 ]
             ]
 
--- app/Widgets.hs
+++ app/Widgets.hs
@@ -3,6 +3,7 @@ module Widgets where
 import           Data.List                   (intercalate, intersperse)
 import qualified Data.Map                    as Map
 import qualified Data.Set                    as S
+import qualified Elements                    as UI
 import           Fuzzy.Base
 import           Fuzzy.FiniteUniversum
 import           Fuzzy.TrapeziumMF
@@ -28,14 +29,13 @@ appHeader = UI.div #+ [UI.h1 # set UI.text "Лабораторна робота 
 
 fuzzySet :: (PrintfArg v, PrintfArg k) => Behavior (FuzzyFiniteUniversum v k, SetName) -> UI (FuzzySet v k)
 fuzzySet bFuzzyData = do
-    section <- UI.div # set UI.style [("display", "flex"), ("flex-direction", "row")]
+    section <- UI.div
     tRow <- UI.tr
     _ <- element section #+ [
-            UI.span # sink UI.text (snd <$> bFuzzyData),
-            UI.table #+ [element tRow]
+            UI.table #. "fuzzy-content pure-table pure-table-bordered" #+ [UI.tbody #+ [element tRow]]
             ]
-    onChanges (fst <$> bFuzzyData) (\fuzzy -> do
-        cells <- makeCells fuzzy
+    onChanges bFuzzyData  (\(fuzzy,sName) -> do
+        cells <- makeCells sName fuzzy
         element tRow # set UI.children cells
         )
     return
@@ -44,11 +44,10 @@ fuzzySet bFuzzyData = do
             fuzzySetTidings = tidings bFuzzyData never
         }
     where
-        makeCells (FuzzyFiniteUniversum fuzzyMap) =
+        makeCells fName (FuzzyFiniteUniversum fuzzyMap) =
             sequence $
-            take 20 $
-            [UI.td # set UI.text (printf "%.2v, %.2v" el mfValue)
-            | (el, mfValue) <- Map.toList fuzzyMap] ++ repeat UI.td
+            (UI.th # set UI.text fName) : take 20 ([UI.td # set UI.text (printf "%.2f, %.2f" el mfValue)
+                                                   | (el, mfValue) <- Map.toList fuzzyMap] ++ repeat UI.td)
 
 fuzzyAB ::
     (PrintfArg v, PrintfArg k) =>
@@ -65,17 +64,22 @@ showSet s = '{': init (tail $ show $ S.toList s) ++ "}"
 
 displaySetInfo :: Behavior VariantData -> UI Element
 displaySetInfo bVarData = do
-    UI.table #+ [
-        UI.tr #+ [
-            UI.th # set UI.text "Множина",
-            UI.th # set UI.text "Висота",
-            UI.th # set UI.text "Мода",
-            UI.th # set UI.text "Носій",
-            UI.th # set UI.text "Ядро",
-            UI.th # set UI.text "Множина альфа рівня"
+    UI.table #. "pure-table pure-table-bordered" #+ [
+        UI.thead #+ [
+            UI.tr #+ [
+                UI.th # set UI.text "Множина",
+                UI.th # set UI.text "Висота",
+                UI.th # set UI.text "Мода",
+                UI.th # set UI.text "Носій",
+                UI.th # set UI.text "Ядро",
+                UI.th # set UI.text "Множина альфа рівня"
+            ]
         ],
-        fSetRow (("A",) . varSetA <$> bVarData),
-        fSetRow (("B",) . varSetA <$> bVarData)
+        UI.tbody #+ [
+            fSetRow (("A",) . varSetA <$> bVarData),
+            fSetRow (("B",) . varSetA <$> bVarData)
+        ]
+
         ]
 
     where fSetRow bFuzzyData =
@@ -118,17 +122,17 @@ trapeziumFuzzyNumberInfo bTrapezia =
 setOps :: Behavior VariantData  -> UI Element
 setOps bVarData  = do
 
-    unionSection <- UI.table
-    intersectionSection <- UI.table
-    compl <- UI.table
+    unionSection <- UI.table #. "pure-table pure-table-bordered"
+    intersectionSection <- UI.table #. "pure-table pure-table-bordered"
+    compl <- UI.table #. "pure-table pure-table-bordered"
     let showSet tabl fName (FuzzyFiniteUniversum fuzzy) = do
-            tableRows <-
-                sequence $
-                (UI.tr #+ [UI.th # set UI.text fName ]) :
-                [UI.tr #+ [UI.td # set UI.text (printf "<%.2f, %.2f>" el mfValue)]
-                | (el, mfValue) <- Map.toList fuzzy]
+            header <- UI.thead #+ [UI.tr #+ [UI.th # set UI.text fName ]]
+            tBody <- UI.tbody #+ [
+                UI.tr #+ [UI.td # set UI.text (printf "<%.2f, %.2f>" el mfValue)]
+                | (el, mfValue) <- Map.toList fuzzy
+                ]
 
-            _ <- element tabl # set UI.children tableRows
+            _ <- element tabl # set UI.children [header,tBody]
 
             return ()
 
@@ -139,10 +143,10 @@ setOps bVarData  = do
         )
 
     UI.div #+ [
-        UI.p # sink UI.text implInfo,
-        element unionSection,
-        element intersectionSection,
-        element compl
+        UI.p #. "pure-u-1-1" # sink UI.text implInfo,
+        UI.div #. "pure-u-6-24" #+ [element unionSection] ,
+        UI.div #. "pure-u-6-24" #+ [element intersectionSection],
+        UI.div #. "pure-u-12-24" #+ [element compl]
         ]
 
 
@@ -152,18 +156,22 @@ setOps bVarData  = do
 
 showComplement :: Element -> FuzzyFiniteUniversum Double Double -> FuzzyFiniteUniversum Double Double -> UI ()
 showComplement tabl (FuzzyFiniteUniversum notA) (FuzzyFiniteUniversum notB) = do
-    tRows <- sequence (UI.tr #+ [
+    h <- UI.thead #+ [
+        UI.tr #+ [
                 UI.th # set UI.text "Доповнення A",
                 UI.th # set UI.text "Доповнення B"
             ]
-            :
-            [UI.tr #+ [
+        ]
+
+    tBody <- UI.tbody #+ [
+            UI.tr #+ [
                 UI.td # set UI.text (printf "<%.2f, %.2f>" elA mfA),
                 UI.td # set UI.text (printf "<%.2f, %.2f>" elB mfB)
-            ] | ((elA, mfA), (elB, mfB)) <- zip (Map.toList notA) (Map.toList notB)]
+            ]
+            | ((elA, mfA), (elB, mfB)) <- zip (Map.toList notA) (Map.toList notB)]
 
-        )
-    _ <- element tabl # set UI.children tRows
+
+    _ <- element tabl # set UI.children [h, tBody]
     return ()
 
 showFuzzy :: SimpleFuzzy -> String
@@ -180,35 +188,41 @@ showTrapezium (TrapeziumMF a b c d) = printf "(%.2f, %.2f, %.2f, %.2f)" a b c d
 
 arithOpSection ::Behavior VariantData -> UI Element
 arithOpSection  bVarData = do
-    UI.table #+ [
-        UI.tr #+ [
-            UI.td,
-            UI.th # set UI.text "Додавання",
-            UI.th # set UI.text "Віднімання",
-            UI.th # set UI.text "Множення",
-            UI.th # set UI.text "Ділення"],
-        UI.tr #+ [
-            UI.td # set UI.text "A і B",
-            UI.td # sink UI.text (showFuzzy . uncurry (+) <$> twoFuzzy),
-            UI.td # sink UI.text (showFuzzy . uncurry (-) <$> twoFuzzy),
-            UI.td # sink UI.text (showFuzzy . uncurry (*) <$> twoFuzzy),
-            UI.td # sink UI.text (showFuzzy . uncurry (/) <$> twoFuzzy)
-            ],
-        UI.tr #+ [
-            UI.td # set UI.text "TriangleA і TriangleB",
-            UI.td # sink UI.text (showTriangle . uncurry (+) <$> twoTriangles),
-            UI.td # sink UI.text (showTriangle . uncurry (-) <$> twoTriangles),
-            UI.td # sink UI.text (showTriangle . uncurry (*) <$> twoTriangles),
-            UI.td # sink UI.text (showTriangle . uncurry (/) <$> twoTriangles)
-            ],
-
-        UI.tr #+ [
-            UI.td # set UI.text "TrapeziumA + TrapeziumB",
-            UI.td # sink UI.text (showTrapezium . uncurry (+) <$> twoTrapezia),
-            UI.td # sink UI.text (showTrapezium . uncurry (-) <$> twoTrapezia),
-            UI.td # sink UI.text (showTrapezium . uncurry (*) <$> twoTrapezia),
-            UI.td # sink UI.text (showTrapezium . uncurry (/) <$> twoTrapezia)
+    UI.table #. "pure-table pure-table-bordered" #+ [
+        UI.thead #+ [
+            UI.tr #+ [
+                UI.td # set UI.text "Множини",
+                UI.th # set UI.text "Додавання",
+                UI.th # set UI.text "Віднімання",
+                UI.th # set UI.text "Множення",
+                UI.th # set UI.text "Ділення"
             ]
+        ],
+        UI.tbody #+ [
+            UI.tr #+ [
+                UI.td # set UI.text "A і B",
+                UI.td # sink UI.text (showFuzzy . uncurry (+) <$> twoFuzzy),
+                UI.td # sink UI.text (showFuzzy . uncurry (-) <$> twoFuzzy),
+                UI.td # sink UI.text (showFuzzy . uncurry (*) <$> twoFuzzy),
+                UI.td # sink UI.text (showFuzzy . uncurry (/) <$> twoFuzzy)
+                ],
+            UI.tr #+ [
+                UI.td # set UI.text "TriangleA і TriangleB",
+                UI.td # sink UI.text (showTriangle . uncurry (+) <$> twoTriangles),
+                UI.td # sink UI.text (showTriangle . uncurry (-) <$> twoTriangles),
+                UI.td # sink UI.text (showTriangle . uncurry (*) <$> twoTriangles),
+                UI.td # sink UI.text (showTriangle . uncurry (/) <$> twoTriangles)
+                ],
+
+            UI.tr #+ [
+                UI.td # set UI.text "TrapeziumA і TrapeziumB",
+                UI.td # sink UI.text (showTrapezium . uncurry (+) <$> twoTrapezia),
+                UI.td # sink UI.text (showTrapezium . uncurry (-) <$> twoTrapezia),
+                UI.td # sink UI.text (showTrapezium . uncurry (*) <$> twoTrapezia),
+                UI.td # sink UI.text (showTrapezium . uncurry (/) <$> twoTrapezia)
+                ]
+        ]
+
         ]
     where
         twoFuzzy = fmap (\VariantData {varSetA=a, varSetB=b} -> (a,b)) bVarData
